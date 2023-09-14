21bc9a5510bcfc044826d31b3a0868a425d832b7
Ishtiak-Ahmed <ishtiak.limon@gmail.com>
2023-09-14 19:35:59 +0600

    Particles Added

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticle;
+    [SerializeField] ParticleSystem deathParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +26,14 @@ public class EnemyDamage : MonoBehaviour
 
     private void KillEnemy()
     {
+        var deathvfx = Instantiate(deathParticle, transform.position, Quaternion.identity);
+        deathvfx.Play();
         Destroy(gameObject);
     }
 
     void ProcessHit()
     {
         hitPoints = hitPoints - 2;
+        hitParticle.Play();
     }
 }
